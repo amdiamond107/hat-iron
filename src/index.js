b00482798335985dc2d5680980f5b0ab44a8b486
dan <dancr.wley@gmail.com>
2023-04-25 11:36:48 -0400

    added 'setUsername' + 'hideDetails' functions to begin ux storyboard

--- src/index.js
+++ src/index.js
@@ -4,12 +4,29 @@ document.getElementById('name-search').style.fontSize="14pt";
 
 
 const nameForm = document.getElementById('name-form')
-nameForm.addEventListener('submit', acceptName)
+nameForm.addEventListener('submit', setUsername)
+const formDiv = document.getElementById('name-entry')
+let username = undefined
 
-function acceptName(e) {
+function setUsername(e) {
     e.preventDefault()
+    let nameInput = document.getElementById('name-search').value
+    username = nameInput
+    console.log(username)
+    nameForm.innerHTML = ''
+    hideDetails()
 
-    let nameInput = document.getElementById
+    // username ? hideDetails : enterUsername
+}
+
+function hideDetails() {
+
+    nameForm.innerHTML = '';
+    const welcomeMessage = document.createElement('h3');
+    welcomeMessage.setAttribute('id','welcome-message');
+    welcomeMessage.textContent = `welcome brother / sister ${username}`
+    
+    formDiv.appendChild(welcomeMessage)
 }
 
 

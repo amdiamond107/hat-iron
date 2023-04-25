33643daf5d6564749f52b72f733e6c26e95271cc
dan crowley <122328258+dan-jpeg@users.noreply.github.com>
2023-04-25 10:49:51 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+document.getElementById('name-search').style.height="40px";
+
+document.getElementById('name-search').style.fontSize="14pt";
+
+
+const nameForm = document.getElementById('name-form')
+nameForm.addEventListener('submit', acceptName)
+
+function acceptName(e) {
+    e.preventDefault()
+
+    let nameInput = document.getElementById
+}
+
+
